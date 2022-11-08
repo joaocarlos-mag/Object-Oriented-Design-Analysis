b9ac21d8b73519763ad84d7caad89b38d76e48bd
JOÃO CARLOS DE MAGALHÃES RODRIGUES <joao.magalhaes@academico.ifrn.edu.br>
2022-11-08 09:01:25 -0300

    CRUD

---
+++ ProjetoApooClinica-master/Persistencia/Contexts/EFContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+namespace Persistencia.Contexts
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CSApoo")
+        {
+            Database.SetInitializer<EFContext>(
+            new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
+        public DbSet<Exame> Exames { get; set; }
+        public DbSet<Consulta> Consultas { get; set; }
+    }
+}
